7d74f742f545997693be0bb31c3d128339f2df36
Jasmine Gutierrez <Jasmineg01@comcast.net>
2025-07-23 11:46:30 -0500

    Vault

---
+++ index.js
@@ -0,0 +1,26 @@
+let code1
+code1 = 7 + 3
+//console.log(code1);
+
+let code2
+code2 = 86 /  2 - 3
+//console.log(code2);
+
+let code3 
+code3 = 53 % 5 * 13
+//console.log(code3);
+
+let message = "The vault has been secured. The combination is:"
+//console.log(message);
+
+let codeA
+codeA = code1 + "-" + code2 + "-" + code3
+//console.log(codeA);
+
+let codeB
+codeB = `${code1}-${code2}-${code3}`
+//console.log(codeB);
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
